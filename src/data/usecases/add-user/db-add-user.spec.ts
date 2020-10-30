537b93f1b83f84515a34bf12274487c6a22fa704
gabrielpittacosta <gabrielpittacosta@gmail.com>
2020-10-30 16:31:24 -0300

    test(DBAddUser): ensure DbAddUser calls with correct value

--- src/data/usecases/add-user/db-add-user.spec.ts
+++ src/data/usecases/add-user/db-add-user.spec.ts
@@ -1,9 +1,10 @@
 import { DbAddUser } from './db-add-user'
-import { Encrypter } from './db-add-user-protocols'
+import { Encrypter, User, AddUserRepository, AddUserModel } from './db-add-user-protocols'
 
 interface SutTypes {
   sut: DbAddUser
   encrypterStub: Encrypter
+  addUserRepositoryStub: AddUserRepository
 }
 
 const makeEncrypter = (): Encrypter => {
@@ -16,13 +17,31 @@ const makeEncrypter = (): Encrypter => {
   return new EncrypterStub()
 }
 
+const makeAddUserRepository = (): AddUserRepository => {
+  class AddUserRepositoryStub implements AddUserRepository {
+    async add (userData: AddUserModel): Promise<User> {
+      const fakeUser = {
+        id: 'valid_id',
+        name: 'valid_name',
+        email: 'valid_email',
+        password: 'hashed_password'
+      }
+
+      return await new Promise(resolve => resolve(fakeUser))
+    }
+  }
+  return new AddUserRepositoryStub()
+}
+
 const makeSut = (): SutTypes => {
   const encrypterStub = makeEncrypter()
-  const sut = new DbAddUser(encrypterStub)
+  const addUserRepositoryStub = makeAddUserRepository()
+  const sut = new DbAddUser(encrypterStub, addUserRepositoryStub)
 
   return {
     sut,
-    encrypterStub
+    encrypterStub,
+    addUserRepositoryStub
   }
 }
 
@@ -43,7 +62,10 @@ describe('DbAddUser UseCase', () => {
 
   test('Should throw is Encrypter throws', async () => {
     const { sut, encrypterStub } = makeSut()
-    jest.spyOn(encrypterStub, 'encrypt').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    jest
+      .spyOn(encrypterStub, 'encrypt')
+      .mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+
     const userData = {
       name: 'valid_name',
       email: 'valid_email',
@@ -52,5 +74,22 @@ describe('DbAddUser UseCase', () => {
     const promise = sut.add(userData)
 
     await expect(promise).rejects.toThrow()
+  })
+
+  test('Should call AddUserRepository with correct values', async () => {
+    const { sut, addUserRepositoryStub } = makeSut()
+    const addSpy = jest.spyOn(addUserRepositoryStub, 'add')
+    const userData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password'
+    }
+    await sut.add(userData)
+
+    expect(addSpy).toHaveBeenCalledWith({
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'hashed_password'
+    })
   })
 })
